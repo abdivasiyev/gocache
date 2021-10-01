5da0d45106d61863161db081edce4fa61562ebbe
Asliddin Azizovich <space.coding.programmer@gmail.com>
2021-10-01 19:38:45 +0500

    added expiration time

--- config.go
+++ config.go
@@ -16,21 +16,23 @@ const (
 
 // Configuration of the package
 type Config struct {
-	Driver    Driver   // driver type
-	Addresses []string // connection address
-	Username  string   // username
-	Password  string   // password
-	DB        int
+	Driver     Driver   // driver type
+	Addresses  []string // connection address
+	Username   string   // username
+	Password   string   // password
+	DB         int      // database
+	Expiration int      // expiration of the cache in seconds
 }
 
 // Construct new configuration
 func New() *Config {
 	return &Config{
-		Driver:    RedisDriver,
-		Addresses: []string{"127.0.0.1:6379"},
-		Username:  "",
-		Password:  "",
-		DB:        0,
+		Driver:     RedisDriver,
+		Addresses:  []string{"127.0.0.1:6379"},
+		Username:   "",
+		Password:   "",
+		DB:         0,
+		Expiration: 3,
 	}
 }
 
@@ -56,5 +58,11 @@ func (c *Config) WithCreds(username, password string) *Config {
 // Change db
 func (c *Config) WithDB(db int) *Config {
 	c.DB = db
+	return c
+}
+
+// Change expiration time
+func (c *Config) WithExpiration(exp int) *Config {
+	c.Expiration = exp
 	return c
 }
