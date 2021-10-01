9f4e9773aaa82a7ace2fdf4431ff91a287834617
Asliddin Azizovich <space.coding.programmer@gmail.com>
2021-10-01 20:45:51 +0500

    memcache client test

--- cache_test.go
+++ cache_test.go
@@ -17,8 +17,24 @@ func TestNewClient(t *testing.T) {
 	assert.Equal(t, nil, client.Close())
 }
 
-func TestSetGet(t *testing.T) {
+func TestRedisSetGet(t *testing.T) {
 	cfg := NewConfig().WithAddr("127.0.0.1:6379").WithDB(0).WithExpiration(4)
+
+	client, err := NewClient(cfg)
+
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, nil, client)
+
+	data := "this is a test data"
+
+	assert.Equal(t, nil, client.Set("test", data))
+	assert.Equal(t, data, client.Get("test"))
+
+	assert.Equal(t, nil, client.Close())
+}
+
+func TestMemcachedSetGet(t *testing.T) {
+	cfg := NewConfig().WithDriver(MemcachedDriver).WithAddr("127.0.0.1:11211").WithDB(0).WithExpiration(4)
 
 	client, err := NewClient(cfg)
 
